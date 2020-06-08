808c4ed7ab0c49b02516d79b3f52f0f77c45d11f
ZeroOne010101 <43586679+ZeroOne010101@users.noreply.github.com>
2020-06-08 23:15:23 +0200

    Possible naming problem fix

--- RegexCollection.py
+++
@@ -1,4 +0,0 @@
-import re
-# The regex compilations are saved in this seperate file for performance reasons
-mainRegex = re.compile(r"^(?::(?P<prefix>\S+) )?(?P<command>\S+)(?: (?!:)(?P<params>.+?))?(?: :(?P<message>.+))?$")
-prefixRegex = re.compile(r"^(?:\S+!\S+@)?(?P<user>\S+).tmi.twitch.tv$") # Twitch specific, ?P<user> is usually the hostname
